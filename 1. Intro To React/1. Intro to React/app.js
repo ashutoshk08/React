dfbb1153725d504c2336b742a4bf934154961b2f
Ashutosh Kumar <kr.ashutosh1202@gmail.com>
2025-08-28 01:13:25 +0530

    Add files via upload

---
+++ 1. Intro To React/1. Intro to React/app.js
@@ -0,0 +1,5 @@
+const element = React.createElement("h1", {}, "Hello Coder Army");
+// ReactDOM.render(element, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
