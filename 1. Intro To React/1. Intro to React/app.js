c1025b6430116729a3f31aa8b88659f62989c753
Ashutosh Kumar <kr.ashutosh1202@gmail.com>
2025-08-28 01:14:00 +0530

    Delete 1. Intro To React directory

--- 1. Intro To React/1. Intro to React/app.js
+++
@@ -1,5 +0,0 @@
-const element = React.createElement("h1", {}, "Hello Coder Army");
-// ReactDOM.render(element, document.getElementById("root"));
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(element);
